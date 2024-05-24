0151a3ef6f3298a17aff91604bd31f1692ddad74
AlexUstosu <44317065+AlexUstosu@users.noreply.github.com>
2024-05-24 19:28:59 +0300

    udate readme and cpp file
    add right version on task_1.cpp

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # patterns
+
+This is repo needs for practice to UML language and patterns. 
+In this database i used next sources:
+
 This is my database about patterns  
-Изучение паттернов проектирования, по моему мнению, должо быть основано на литературе, которая корнями уходит зарождение этой дисциплины. Данный репозиторий будет дополняться по мере изучения книги "Паттерны объектно-ориентированного проектирования" Э.Гамма, Р.Хельм, Р.Джонсон, Дж.Влиссидес 
+"Паттерны объектно-ориентированного проектирования" Э.Гамма, Р.Хельм, Р.Джонсон, Дж.Влиссидес 
+
+In folder "task_from_design_on_UML" i tried solve tasks in book Антон Хританков "Проектирование на UML. Сборник задач"
+In folder "tasks_from_PTOOP" i tried solve tasks in book https://ptaskbook.com/ru/ptforoop/index.php
--- tasks_from_PTOOP/task_1.cpp
+++ tasks_from_PTOOP/task_1.cpp
@@ -1,6 +1,72 @@
-﻿//Базовые принципы ООП
+﻿//Базовые принципы ООП 
+//Задача OOP0Begin1
 #include <iostream>
 
+using namespace std;
+
+//Stack Node
+class Node
+{ 
+public:
+    int _data;
+    shared_ptr<Node> _next;
+
+    Node(int data, shared_ptr<Node> next) : _data{ data }, _next{ next } {};
+};
+
+//Class of Collection
+class Stack
+{
+private:
+    shared_ptr<Node> _top = nullptr;
+public:
+	Stack();
+	~Stack();
+
+    bool isEmpty();
+    void push(int data);
+    int pop();
+    int peek();
+    void clear();
+    string toString();
+
+};
+
+Stack::Stack()
+{
+}
+
+Stack::~Stack()
+{
+}
+
+bool Stack::isEmpty()
+{
+    return false;
+}
+
+void Stack::push(int data)
+{
+}
+
+int Stack::pop()
+{
+    return 0;
+}
+
+int Stack::peek()
+{
+    return 0;
+}
+
+void Stack::clear()
+{
+}
+
+string Stack::toString()
+{
+    return string();
+}
 
 int main()
 {
